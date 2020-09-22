091ddf39a3ffa200f39f0838944b7388470fb17f
Erwins Saget <erwinssaget@gmail.com>
2020-09-22 19:55:26 -0400

    Update template file with leaflet

---
+++ static/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoibXJ1Y2tlcjgxMyIsImEiOiJja2V1aW80ZnkwcTQzMnlvMHoxN2xxejU5In0.zsIP2dJ6SC89MbBKSfOwpQ";
---
+++ static/logic.js
@@ -0,0 +1,36 @@
+// Create a map object
+var myMap = L.map("map", {
+  center: [37.09, -95.71],
+  zoom: 5
+});
+
+// Add a tile layer
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+// An array containing each city's name, location, and population
+var cities = [{
+  location: [40.7128, -74.0059],
+  name: "33626",
+  population: "8,550,405"
+},
+{
+  location: [41.8781, -87.6298],
+  name: "33629",
+  population: "2,720,546"
+},
+];
+
+// Loop through the cities array and create one marker for each city, bind a popup containing its name and population add it to the map
+for (var i = 0; i < cities.length; i++) {
+  var city = cities[i];
+  L.marker(city.location)
+    .bindPopup("<h1>" + city.name + "</h1> <hr> <h3>Population " + city.population + "</h3>")
+    .addTo(myMap);
+}
